5445839302b5caef66fc0697542735a24fdbe9a8
NguyenLeVy13 <2014509@dlu.edu.vn>
2023-03-01 14:46:24 +0700

    Initial first project Lab01

---
+++ src/TipsAndTricks/TatBlog.Core/Entities/Tag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Contracts;
+
+namespace TatBlog.Core.Entities
+{
+    // Biểu diễn một từ khóa trong bài viết
+    public class Tag : IEntity
+    {
+        // Mã từ khóa 
+        public int Id { get; set; }
+
+        // Nội dung từ khóa
+        public string Name { get; set; }
+
+        // Tên định danh để tạo URL
+        public string UrlSlug { get; set; }
+
+        // Mô tả thêm về từ khóa 
+        public string Description { get; set; }
+
+        //Danh sách bài viết có chứa từ khóa
+        public IList<Post> Posts { get; set; }
+    }
+}
---
+++ src/TipsAndTricks/TatBlog.Data/Mappings/AuthorMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Data.Mappings
+{
+    public class AuthorMap : IEntityTypeConfiguration<Author>
+    {
+    }
+}
