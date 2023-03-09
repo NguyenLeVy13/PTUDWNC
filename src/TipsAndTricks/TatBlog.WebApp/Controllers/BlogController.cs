0d4795391b5af1776c7ed2131e967afc6d0f0643
NguyenLeVy13 <2014509@dlu.edu.vn>
2023-03-09 19:44:02 +0700

    Lab02_B12

--- src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/BlogController.cs
@@ -11,7 +11,11 @@ namespace TatBlog.WebApp.Controllers
         {
             _blogRepository = blogRepository;
         }
+
+        //Action này xử lý HTTP request đến trang chủ của
+        //ứng dụng web hoặc tìm kiếm bài viết theo từ khoá
         public async Task<IActionResult> Index(
+            [FromQuery(Name = "k")] string keyword = null,
             [FromQuery(Name = "p")] int pageNumber = 1,
             [FromQuery(Name = "ps")] int pageSize = 10)
         {
@@ -19,7 +23,10 @@ namespace TatBlog.WebApp.Controllers
             var postQuery = new PostQuery()
             {
                 //Chỉ lấy những bài viết có trạng thái Published
-                PublishedOnly = true
+                PublishedOnly = true,
+
+                //Tìm bài viết theo từ khóa
+                Keyword = keyword
             };
 
             //Truy vấn các bài viết theo đk đã tạo
