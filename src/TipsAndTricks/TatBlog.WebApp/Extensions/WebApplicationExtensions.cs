81e62895c52c6a66e6d28600ee6840e7bd4c714f
NguyenLeVy13 <2014509@dlu.edu.vn>
2023-03-10 14:15:39 +0700

    Lab02_B14

---
+++ src/TipsAndTricks/TatBlog.WebApp/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,90 @@
+﻿using TatBlog.Data.Contexts;
+using System.Runtime.CompilerServices;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+using Microsoft.EntityFrameworkCore;
+
+namespace TatBlog.WebApp.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        //Thêm các dịch vụ được yêu cầu bởi MVC Framework
+        public static WebApplicationBuilder ConfigureMvc(
+            this WebApplicationBuilder builder) 
+        { 
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddResponseCompression();
+
+            return builder;
+        }
+
+        //Đăng ký các dịch vụ với DI Container
+        public static WebApplicationBuilder ConfigureServices(
+            this WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<BlogDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration
+                        .GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+            builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+
+            return builder;
+        }
+
+        public static WebApplication UseRequestPipeLine(
+            this WebApplication app)
+        {
+            //Thêm middleware để hiển thị thông báo lỗi
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Blog/Error");
+
+                //Thêm middleware cho việc áp dụng HSTS (thêm header
+                //Strict-Transpost-Security vào HTTP Response).
+                app.UseHsts();
+            }
+
+            //Thêm middleware để chuyển hướng HTTP sang HTTP
+            app.UseHttpsRedirection();
+
+            //Thêm middleware phục vụ các yêu cầu liên quan
+            //tới các tập tin nội dung tĩnh như hình ảnh, css, ...
+            app.UseStaticFiles();
+
+            //Thêm middleware lựa chọn endpoint phù hợp nhất
+            //để xử lý một HTTP request.
+            app.UseRouting();
+
+            return app;
+        }
+
+        //Thêm dữ liệu mẫu vào CSDL
+        public static IApplicationBuilder UseDataSeeder(
+            this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+
+            try
+            {
+                scope.ServiceProvider
+                    .GetRequiredService<IDataSeeder>()
+                    .Initialize();
+            }
+            catch (Exception ex)
+            {
+
+                scope.ServiceProvider
+                    .GetRequiredService<ILogger<Program>>()
+                    .LogError(ex, "Could not insert dat into database");
+            }
+
+            return app;
+        }
+    }
+}
--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -2,80 +2,27 @@
 using TatBlog.Data.Seeders;
 using TatBlog.Services.Blogs;
 using Microsoft.EntityFrameworkCore;
+using TatBlog.WebApp.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 {
-    //Thêm các dịch vụ được yêu cầu bởi MVC Framework
-    builder.Services.AddControllersWithViews();
 
-    //Đăng ký các dịch vụ với DI Container
-    builder.Services.AddDbContext<BlogDbContext>(options =>
-        options.UseSqlServer(
-            builder.Configuration.GetConnectionString("DefaultConnection")));
-
-    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+    builder
+        .ConfigureMvc()
+        .ConfigureServices(); 
 }
 
 var app = builder.Build();
 {
-    //Cấu hình HTTP Request pipeline
-
-    //Thêm middleware để hiển thị thông báo lỗi
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Blog/Error");
-
-        //Thêm middleware cho việc áp dụng HSTS (thêm header
-        //Strict-Transpost-Security vào HTTP Response).
-        app.UseHsts();
-    }
-
-    //Thêm middleware để chuyển hướng HTTP sang HTTP
-    app.UseHttpsRedirection();
-
-    //Thêm middleware phục vụ các yêu cầu liên quan
-    //tới các tập tin nội dung tĩnh như hình ảnh, css, ...
-    app.UseStaticFiles();
-
-    //Thêm middleware lựa chọn endpoint phù hợp nhất
-    //để xử lý một HTTP request.
-    app.UseRouting();
-
-    //Định nghĩa route template, route constraint cho các
-    //endpoints kết hợp với các action trong các controller.
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Blog}/{action=Index}/{id?}");
-
-    app.MapControllerRoute(
-        name: "posts-by-category",
-        pattern: "blog/category/{slug}",
-        defaults: new { controller = "Blog", action = "Category" });
-
-    app.MapControllerRoute(
-        name: "posts-by-tag",
-        pattern: "blog/tag/{slug}",
-        defaults: new { controller = "Blog", action = "Tag" });
-
-    app.MapControllerRoute(
-        name: "single-post",
-        pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
-        defaults: new { controller = "Blog", action = "Post" });
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Blog}/{action=Index}/{id?}");
-
+    app.UseRequestPipeLine();
+    app.UseBlogRoutes();
+    app.UseDataSeeder();
 }
 
-//Thêm dữ liệu mẫu vào CSDL
+/*//Thêm dữ liệu mẫu vào CSDL
 using (var scope = app.Services.CreateScope())
 {
     var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-}
+}*/
 
 app.Run();
