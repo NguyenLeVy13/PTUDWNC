b12791f67f2ddf366a662374554da2d9cc0679a9
NguyenLeVy13 <2014509@dlu.edu.vn>
2023-03-15 10:29:49 +0700

    Lab03_B1

---
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TatBlog.WebApp.Areas.AdminArea.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
